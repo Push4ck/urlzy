82a0d21bf52de6b7335e7048236813e0a58f7328
Push4ck <pushkarsharma652@gmail.com>
2025-09-02 14:47:05 +0530

    node server fix

--- server/server.js
+++ server/server.js
@@ -24,8 +24,6 @@ mongoose
   .connect(
     process.env.MONGODB_URI || "mongodb://localhost:27017/urlshortener",
     {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 20000,
     }
   )
